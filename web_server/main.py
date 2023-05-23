be2a5535f0fd8a2fca7d3b6a6f6d046399be6038
Daniel Méndez <daphni_mgld@hotmail.com>
2023-05-22 23:51:18 -0600

    feat: first approach to fastapi framework using virtual environments

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,21 @@
 import api
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+  return {"Hello": "World"}
+
+
+@app.get("/contact", response_class=HTMLResponse)
+def read_root():
+  return """
+    <h1>Contact us:</h1>
+    <p>Email: test@gmail.com</p>
+  """
 
 
 def run():
